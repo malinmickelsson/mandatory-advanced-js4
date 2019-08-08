1ff7b54bb236046548ff3514afa4fee8e9396f4e
Malin Larsson <malin.larsson@utb.ecutbildning.se>
2019-08-08 11:14:55 +0200

    building the board

--- connect4/src/store.js
+++ connect4/src/store.js
@@ -2,12 +2,22 @@ import {createStore} from 'redux';
 
 //Initial state for my store
 const initial =  {
-    
+    current: 'red', // kan även vara blå
+    board: [ // 7 col X 6 rows
+        [], // col 1
+        [], // col 2
+        [], // col 3
+        [], // col 4
+        [], // col 5
+        [], // col 6
+        [], // col 7
+    ],
 };
 
 function reducer(state, action) {
     if (action.type === 'DROP_TILE') {
         //do a thing
+        console.log('landing in column ' + action.payload);
     }
 
     return state;
